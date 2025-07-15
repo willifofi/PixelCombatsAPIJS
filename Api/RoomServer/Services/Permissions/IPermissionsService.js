d65e09c0a98c816e88eb99e7e539595df597e53e
willi <kostiaivsnov555@gmail.com>
2025-07-15 18:00:40 +0300

    Update and rename IPermissionsService.cs to IPermissionsService.js

---
+++ Api/RoomServer/Services/Permissions/IPermissionsService.js
@@ -0,0 +1,16 @@
+// ветка room_api.Permissions - сервис разрешений в игровом режима
+// (структура разрешений в комнате)
+import { Permissions } from "pixel_combats/room";
+
+// имя файла разрешений ("client/room/permissions.json")
+Permissions.GAME_MODE_ROOM_PERMISSIONS;
+
+// разрешения в комнате	
+//(чтобы разрешения в комнате были нестандартные (Permissions.Value.Default)
+// нужно при создании режима создать файл Permissions.GAME_MODE_ROOM_PERMISSIONS)	
+const params = Permissions.Value;
+	
+// можно ли закрывать комнату		
+params.CloseRoom; // bool (true, false)	
+// разрешения в комнате по умолчанию	
+params.Default;
