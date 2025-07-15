f8dad493e7c526b133d249655b95e31759122c0f
willi <kostiaivsnov555@gmail.com>
2025-07-15 18:32:39 +0300

    Update ILeaderBoardService.js

--- Api/RoomServer/Services/LeaderBoard/ILeaderBoardService.js
+++ Api/RoomServer/Services/LeaderBoard/ILeaderBoardService.js
@@ -1,43 +1,34 @@
 // ветка room_api.LeaderBoard
 import { LeaderBoard } from "pixel_combats/room";
+// ветка basic_api.DisplayValueHeader
+import { DisplayValueHeader } from "pixel_combats/basic";
+	
+// имя значения (propertiesId)
+const VALUE = "ExampleName";
+// выводимый заголовок (если null тогда VALUE)
+const DISPLAY_NAME = "ExampleDisplayName";
+const SHORT_DISPLAY_NAME = "ExampleShortDisplayName";
+// сокращенное выводимое значение (если null тогда SHORT_DISPLAY_NAME)
+const displayValueHeader = new DisplayValueHeader(VALUE, DISPLAY_NAME, SHORT_DISPLAY_NAME);
 
-
+// --- лидерборд ---
+		
+// имя значения, которые следует выводить в лидерборде для игрока
 LeaderBoard.PlayerLeaderBoardValues = [
-	new DisplayValueHeader("")
+	new DisplayValueHeader(VALUE, DISPLAY_NAME, SHORT_DISPLAY_NAME);
 ];
+// имя значение, которое необходимо выводить для лидерборда команд		
+LeaderBoard.TeamLeaderBoardValue = new DisplayValueHeader(VALUE, DISPLAY_NAME, SHORT_DISPLAY_NAME);
 
-// функция сравнения игроков
-		/// <para>Принимает игрока, возвращает значение веса игрока в лидерборде.</para>
-		/// </summary>
-		ApiFunc<IPlayerApi, int> PlayersWeightGetter { get; set; }
-		/// <summary>
-		/// функция сравнения команд
-		/// <para>Принимает команду, возвращает значение веса команды в лидерборде.</para>
-		/// </summary>
-		ApiFunc<ITeamApi, int> TeamWeightGetter { get; set; }
+// функция сравнения команд (Принимает команду, возвращает значение веса команды в лидерборде)
+LeaderBoard.TeamWeightGetter.Set(team => team.Properties.Get("propId").Value);
+// функция сравнения игроков (Принимает игрока, возвращает значение веса игрока в лидерборде)
+LeaderBoard.PlayersWeightGetter.Set(player => player.Properties.Get("propId").Value);
+	
+// возвращает лидерборд команд в комнате		
+LeaderBoard.GetTeams();	
 
-		/// <summary>
-		/// возвращает всех игроков в команде, в отсортированном порядке
-		/// </summary>
-		/// <param name="team">команда</param>
-		IList<IPlayerApi> GetPlayers(ITeamApi team);
-
-		/// <summary>
-		/// имя значения, которые следует выводить в лидерборде для игрока
-		/// </summary>
-		DisplayValueHeader[] PlayerLeaderBoardValues { get; set; }
-		/// <summary>
-		/// имя значение, которое необходимо выводить для лидерборда команд
-		/// </summary>
-		DisplayValueHeader TeamLeaderBoardValue { get; set; }
-
-		/// <summary>
-		/// возвращает лидерборд команд в комнате
-		/// </summary>
-		IList<LeaderBoardTeamItem> GetTeams();
-		/// <summary>
-		/// возвращает лидерборд игроков в комнате
-		/// </summary>
-		IList<LeaderBoardPlayerItem> GetPlayers();
-	}
-}
+// возвращает лидерборд игроков в комнате	
+LeaderBoard.GetPlayers();
+// в отсортированном порядке (team - команда)	
+LeaderBoard.GetPlayers(ITeamApi team);	
