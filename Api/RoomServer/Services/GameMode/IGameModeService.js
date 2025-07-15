03f44e554b333d9880bcefd2969689a5af594e8d
willi <kostiaivsnov555@gmail.com>
2025-07-15 17:51:29 +0300

    Update and rename IGameModeService.cs to IGameModeService.js

---
+++ Api/RoomServer/Services/GameMode/IGameModeService.js
@@ -0,0 +1,9 @@
+// ветка room_api.GameMode - сервис игрового режима
+// (хранит значения параметров игрового режима)
+import { GameMode } from "pixel_combats/room";
+
+// текущий игровой режим (string)
+GameMode.Value;
+
+// значения параметров (object)
+GameMode.Parameters;
