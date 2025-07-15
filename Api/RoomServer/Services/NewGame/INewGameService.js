f4b50cc03905354550ac671bf4ced7db7fc4d755
willi <kostiaivsnov555@gmail.com>
2025-07-15 19:43:51 +0300

    Update and rename INewGameService.cs to INewGameService.js

---
+++ Api/RoomServer/Services/NewGame/INewGameService.js
@@ -0,0 +1,10 @@
+// ветка room_api.NewGame - сервис управления новой игрой
+import { NewGame } from 'pixel_combats/basic';
+
+		
+// стандартный перезапуск игры	
+NewGame.RestartGame();
+		
+// перезапуск игры с указанными параметрами новой игры	
+// data - данные о запуске новой игры			
+NewGame.RestartGame(data);
