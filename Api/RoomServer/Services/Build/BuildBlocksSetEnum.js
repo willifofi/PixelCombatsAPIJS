b61172379c4a51689b507681d2dd31dad92763d6
willi <kostiaivsnov555@gmail.com>
2025-07-17 04:50:13 +0300

    Update BuildBlocksSetEnum.js

--- Api/RoomServer/Services/Build/BuildBlocksSetEnum.js
+++ Api/RoomServer/Services/Build/BuildBlocksSetEnum.js
@@ -1,19 +1,32 @@
 // набор всех пресетов наборов блоков
 import { BuildBlocksSet } from "pixel_combats/room";
 
-
 // пустой набор блоков		
-IBlocksSet NullSet { get; }
+const nullSet = BuildBlocksSet.NullSet;
 // набор всех блоков				
-IBlocksSet All { get; }
+const allSet = BuildBlocksSet.All;
 // набор всех чистых блоков (которые не сломанные)		
-IBlocksSet AllClear { get; }
-// набор блоков для красной команды
-IBlocksSet Blue { get; }
+const allClearSet = BuildBlocksSet.AllClear;
 // набор блоков для синей команды
-IBlocksSet Red { get; }
+const blueSet = BuildBlocksSet.Blue;
+// набор блоков для красной команды
+const RedSet = BuildBlocksSet.Red;
 
 // возвращает набор блоков по его ID
 // (если набора с указанным ID не существует то выдаст пустой набор)
 // 0 - ID набора блоков
-IBlocksSet Get(0);
+const blockSetById = BuildBlocksSet.Get(0);
+
+// --- обьект набора блока ---
+// дупустим набор блоков All
+const b_set = allSet;
+// идентиффикатор набора блоков
+const set_id = b_set.Id;
+// сколько всего блоков в наборе
+const set_count = b_set.Count;
+// проверяет наличие в наборе указанного блока
+// 0 - ID блока</param>
+const set_cont = b_set.Contains(0);
+// возвращает блок по его индексу
+// 0 - индекс блока
+const block = b_set.GetByIndex(0);
